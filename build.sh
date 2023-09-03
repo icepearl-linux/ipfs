95a7be953ff7e71a9eac784245c15b9a81e4baa4
Aldrin Balitbit <aldrinbalitbit1127@gmail.com>
2023-09-03 09:44:51 +0800

    [?1049h[22;0;0t[1;42r[4l[?25l(B[m[37m[40m[1;42r[H[2J[1;1H[97m[44m[K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K [K[1;1H[?25l[?12l[?25h[17;15H[30m[47m┌──────────────────┤ [31mMirror push commit[30m ├──────────────────┐[18;15H│ Enter your commit message to push icepearl (GitLab) and  │[97m[40m [19;15H[30m[47m│ icepearl-mirror (GitHub):                                │[97m[40m [20;15H[30m[47m│                                                          │[97m[40m [21;15H[30m[47m│ [37m[44m________________________________________________________[30m[47m │[97m[40m [22;15H[30m[47m│                                                          │[97m[40m [23;15H[30m[47m│                                                          │[97m[40m [24;15H[30m[47m│              <Ok>                  <Cancel>              │[97m[40m [25;15H[30m[47m│                                                          │[97m[40m [26;15H[30m[47m└──────────────────────────────────────────────────────────┘[97m[40m [27;16H                                                            [21;17H[?25l[?12l[?25h[42;1H[37m(B[m[39;49m[K[?1049l[23;0;0t

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@ DIALOG_ESC=255
 while true
 do
     exec 3>&1
-    selection=$(dialog --title "IPFS Build Menu" \
-	               --menu "Execute the build script" 0 0 10 \
-		       "1" "Build and install to hard disk" \
-		       "2" "Build rootfs tarball" \
-		       "3" "Build ISO" \
-		       2>&1 1>&3)
+    selection=$(whiptail --title "IPFS Build Menu" \
+	                 --menu "Execute the build script" 0 0 10 \
+		         "1" "Build and install to hard disk" \
+		         "2" "Build rootfs tarball" \
+		         "3" "Build ISO" \
+		         2>&1 1>&3)
     exit_status=$?
     exec 3>&-
-    clear
     case $exit_status in
 	$DIALOG_CANCEL) exit ;;
 	$DIALOG_ESC) exit 1 ;;
